4df01e04f26205c9154a19d3544d38703487c799
Vlad Romanov <romvlad2002@gmail.com>
2023-04-21 14:52:31 +0300

    Stage2

--- 1/lab1.java
+++ 1/lab1.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+
 
 public class lab1{
     public static void main(String[] args) {
@@ -8,51 +10,76 @@ public class lab1{
                 FileOutputStream fos=new FileOutputStream("leasing_new.txt"))
         {
             byte[] buffer = new byte[256];
+           
+            ArrayList<Byte> buf = new ArrayList<Byte>();
              
             int count;
             // считываем буфер
             while((count=fin.read(buffer))!=-1){
-                if (count == ' ')
-                    count = '!';
+                for(int i=0; i<count;i++){
+             
+                
+                    buf.add(buffer[i]);
+                    if ((char)buffer[i] == ' ') {
+                    
+                        buf.add((byte)'\b');
+                        buf.add((byte)' ');
+                    }
+            
+                }
+                byte[] buffer2 = new byte[buf.size()];
+                for (Byte byt : buf) {
+                    for (int i=0;i<buf.size();i++){
+                        buffer2[i] = byt;
+                    }
+                }
+                // for(int j=0; j<buf.size();j++){
+             
+                //     if (buf.get(j) == 32){
+                //         buf.add(j+1,(byte)'\b');
+                //         buf.add(j+2,(byte)' ');
+                //     }
+                    
+                // }
+                    
                 // записываем из буфера в файл
-                fos.write(buffer, 0, count);
+                fos.write(buffer2, 0, buf.size());
+                
             }
-            System.out.println("File has been written");
+            // ObjectOutputStream oos = new ObjectOutputStream(fos);
+            // oos.writeObject(buf);
+            // fos.close();
+            
+            // System.out.println("File has been written");
+            // System.out.println(encrypt(".\\leasing.txt"));
+            // System.out.println(encrypt(".\\leasing_new.txt"));
         }
         catch(IOException ex){
               
             System.out.println(ex.getMessage());
         } 
+        
+        
+        
+    }
 
-        // ProcessBuilder builder = new ProcessBuilder();
-        // builder.command("cmd.exe", "/c", "openssl dgst -sha1 .\\leasing.txt");
-        // try {
+    private static String encrypt(String filePath) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "openssl dgst -sha1 " + filePath);
+            Process process = pb.start();
 
-        //     Process process = builder.start();
-    
-        //     StringBuilder output = new StringBuilder();
-    
-        //     BufferedReader reader = new BufferedReader(
-        //             new InputStreamReader(process.getInputStream()));
-    
-        //     String line;
-        //     while ((line = reader.readLine()) != null) {
-        //         output.append(line + "\n");
-        //     }
-    
-        //     int exitVal = process.waitFor();
-        //     if (exitVal == 0) {
-        //         System.out.println("Success!");
-        //         System.out.println(output);
-        //         System.exit(0);
-        //     } else {
-        //         //abnormal...
-        //     }
-    
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        // }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line, result = "";
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                result = line.split("= ")[1];
+            }
+
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }

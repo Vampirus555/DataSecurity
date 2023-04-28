694750f999a9ff01b01f236dfe185b3e9d5676cc
Vlad Romanov <romvlad2002@gmail.com>
2023-04-28 18:44:40 +0300

    some changes

--- 2/lab2.java
+++ 2/lab2.java
@@ -5,29 +5,29 @@ import java.util.*;
 public class lab2{
 
     public static void main(String[] args) {
+        
         String originalImagePath = new File("28.bmp").getPath();
         String modifiedImagePath = new File("28embed.bmp").getPath();
         String filePath = new File("leasing.txt").getPath();
 
-        String hex = encrypt(filePath);
-        String secretMessage = "";
-        
-        //Преобразование строки в массив байтов
-        
-        byte[] stringBytes = HexStringToByteArray(hex);
-        
-        
 
-        // Преобразование масcива байтов в строку битов
-        for (int i = 0; i < stringBytes.length; i++) {
-            secretMessage += String.format("%8s", Integer.toBinaryString(stringBytes[i] & 0xFF)).replace(' ', '0');
-        }
-        System.out.println("Хэш: " + hex);
-        System.out.println("Биты>: " + secretMessage);
-        
-        int messageIndex = 0;
-        
         try {
+            String hex = encrypt(filePath); // получение hash кода из файла
+            String secretMessage = ""; // стркоа битов
+            
+            //Преобразование строки в массив байтов
+            byte[] stringBytes = HexStringToByteArray(hex);
+            
+            // Преобразование масcива байтов в строку битов
+            for (int i = 0; i < stringBytes.length; i++) {
+                secretMessage += String.format("%8s", Integer.toBinaryString(stringBytes[i] & 0xFF)).replace(' ', '0');
+            }
+
+            // System.out.println("Хэш: " + hex);
+            // System.out.println("Биты>: " + secretMessage);
+            
+
+            int messageIndex = 0; 
             // Чтение исходного файла изображения
             FileInputStream inputStream = new FileInputStream(originalImagePath);
             byte[] header = new byte[122];
@@ -35,7 +35,6 @@ public class lab2{
             byte[] imageBytes = new byte[inputStream.available()];
             inputStream.read(imageBytes);
             inputStream.close();
-
 
             // Генерирация ключа
             int[] key = generateKey(secretMessage.length(), 0, imageBytes.length);
@@ -46,21 +45,20 @@ public class lab2{
                     // Получаем двоичное представление текущего байта
                     String binaryByte = String.format("%8s", Integer.toBinaryString(imageBytes[key[i]] & 0xFF)).replace(' ', '0');
                     
-                    // Заменим младший значащий бит текущего байта следующим битом секретного сообщения
+                    // Заменяем младший значащий бит текущего байта следующим битом секретного сообщения
                     char[] binaryCharArray = binaryByte.toCharArray();
                     binaryCharArray[7] = secretMessage.charAt(messageIndex % secretMessage.length());
-                    binaryByte = new String(binaryCharArray);
-                    
+                    binaryByte = new String(binaryCharArray);   
                     
                     // Установим измененный байт обратно в байты изображения
                     imageBytes[key[i]] = (byte) Integer.parseInt(binaryByte, 2);
                     
-                    // Увеличим индекс сообщения
+                    // Увеличим индекс сообщения (для взамодействия со следующим символом строки)
                     messageIndex++;
                 }
             }
             
-            // Запишем измененные байты изображения в новый файл
+            // Запишем шапку изображения и измененные байты изображения в новый файл
             FileOutputStream outputStream = new FileOutputStream(modifiedImagePath);
             outputStream.write(header);
             outputStream.write(imageBytes);
@@ -95,11 +93,11 @@ public class lab2{
             
             byte[] newStringBytes = getBytes(retrievedMessage);
 
-            String hashRetrived = byteArrayToHexString(newStringBytes);
+            String hexRetrived = byteArrayToHexString(newStringBytes);
             
-            // Output the original and retrieved message to the console
+            
             System.out.println("Оригинальное сообщение: " + hex);
-            System.out.println("Извлеченное сообщение: " + hashRetrived);
+            System.out.println("Извлеченное сообщение: " + hexRetrived);
             
         } catch (IOException e) {
             e.printStackTrace();

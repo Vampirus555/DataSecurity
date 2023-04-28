71a2c4b6ecf6c675f89afa39dd9c8ab2b55959fa
Vlad Romanov <romvlad2002@gmail.com>
2023-04-28 17:15:47 +0300

    Stage2

--- 2/lab2.java
+++ 2/lab2.java
@@ -1,8 +1,9 @@
 import java.io.*;
-
+import java.util.*;
 
 
 public class lab2{
+
     public static void main(String[] args) {
         String originalImagePath = new File("28.bmp").getPath();
         String modifiedImagePath = new File("28embed.bmp").getPath();
@@ -34,12 +35,16 @@ public class lab2{
             byte[] imageBytes = new byte[inputStream.available()];
             inputStream.read(imageBytes);
             inputStream.close();
+
+
+            // Генерирация ключа
+            int[] key = generateKey(secretMessage.length(), 0, imageBytes.length);
             
             // Вставим секретное сообщение в байты изображения, используя метод замены LSB
-            for (int i = 0; i < imageBytes.length; i++) {
+            for (int i = 0; i < key.length; i++) {
                 if (messageIndex < secretMessage.length()) {
                     // Получаем двоичное представление текущего байта
-                    String binaryByte = String.format("%8s", Integer.toBinaryString(imageBytes[i] & 0xFF)).replace(' ', '0');
+                    String binaryByte = String.format("%8s", Integer.toBinaryString(imageBytes[key[i]] & 0xFF)).replace(' ', '0');
                     
                     // Заменим младший значащий бит текущего байта следующим битом секретного сообщения
                     char[] binaryCharArray = binaryByte.toCharArray();
@@ -48,7 +53,7 @@ public class lab2{
                     
                     
                     // Установим измененный байт обратно в байты изображения
-                    imageBytes[i] = (byte) Integer.parseInt(binaryByte, 2);
+                    imageBytes[key[i]] = (byte) Integer.parseInt(binaryByte, 2);
                     
                     // Увеличим индекс сообщения
                     messageIndex++;
@@ -60,6 +65,8 @@ public class lab2{
             outputStream.write(header);
             outputStream.write(imageBytes);
             outputStream.close();
+
+            
             
             // Чтение измененного файл изображения
             inputStream = new FileInputStream(modifiedImagePath);
@@ -72,10 +79,10 @@ public class lab2{
             // Извлечение секретного сообщения из байтов измененного изображения, используя метод замены LSB
             StringBuilder messageBuilder = new StringBuilder();
             messageIndex = 0;
-            for (int i = 0; i < modifiedImageBytes.length; i++) {
+            for (int i = 0; i < key.length; i++) {
                 if (messageIndex < secretMessage.length()) {
                     // Получаем двоичное представление текущего байта
-                    String binaryByte = String.format("%8s", Integer.toBinaryString(modifiedImageBytes[i] & 0xFF)).replace(' ', '0');
+                    String binaryByte = String.format("%8s", Integer.toBinaryString(modifiedImageBytes[key[i]] & 0xFF)).replace(' ', '0');
                     
                     // Извлекаем наименее значимый бит текущего байта и добавьте его в секретное сообщение
                     messageBuilder.append(binaryByte.charAt(7));
@@ -150,6 +157,25 @@ public class lab2{
         for (int i = 0; i < b.length; i++) {
             result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
         }
+        return result;
+    }
+
+    public static int[] generateKey(int count, int min, int max) {
+
+        int[] result = new int[count];
+        Set<Integer> keySet = new HashSet<>();
+
+        Random random = new Random();
+        int i = 0;
+        while (i < count) {
+            int pos = random.nextInt(max - min + 1) + min;
+            if (!keySet.contains(pos)) {
+                keySet.add(pos);
+                result[i] = pos;
+                i++;
+            }
+        }
+
         return result;
     }
 }

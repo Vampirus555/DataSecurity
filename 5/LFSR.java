29d5d763ffddd95f650e591cb9e4c71e938569c7
Vlad Romanov <romvlad2002@gmail.com>
2023-05-27 00:59:05 +0300

    Lab5_final

--- 5/LFSR.java
+++ 5/LFSR.java
@@ -1,33 +1,70 @@
+import java.util.Arrays;
+
 public class LFSR {
-    
-    private int tapPositions; // Позиции отводов (задаются полиномом)
+    private int[] tapPositions; // Позиции отводов (задаются полиномом)
+    private int[] register; // Значения регистра
 
-    private int register; // Значения регистра
-    
-
-    public LFSR(int tapPositions, int register) {
-        this.tapPositions = tapPositions;
-        this.register = register;
-        
+    public LFSR(int[] polynomial, int[] initialRegister) {
+        this.tapPositions = Arrays.copyOf(polynomial, polynomial.length);
+        this.register = Arrays.copyOf(initialRegister, initialRegister.length);
     }
 
     public int getNextBit() {
-        int digits = (int) Math.log10(register) + 1; //определяем количество цифр в регистре
-
-        int output = register & (1 << (digits - 1)); //получаем число, у которогопоследний бит будет равен последнему нашего регистра
-        output = output >> (digits - 1); //сдвигаем полученный бит в начало, чтобы получить фактический последний бит регистра
-
-        // Если последний бит равен 1, то сдвигаем биты регистра вправо, вставляя в старший бит 1 и выполняем XOR
-        if (output == 1){
-            register >>>= 1 | 0x1;
-            register = register ^ tapPositions;
-        }
-        // если 0, то просто сдвигаем биты, а новый старший бит заполнится нулем
-        else if(output == 0) register >>>= 1;
+        // Сохраняем последний бит регистра (выходной)
+        int outputBit = register[register.length - 1];
         
-        // возвращаем последний бит
-        return output;
+        // Производим сдвиг регистра путем использования arraycopy
+        System.arraycopy(register, 0, register, 1, register.length - 1);
+        
+        // В саммое начало вставляем сохраненный последний бит регистра
+        register[0] = outputBit;
+        
+        // Если выходной бит == 1, то биты отвода меняют своё значение на противоположное, и все биты сдвигаются вправо
+        if (outputBit == 1) {
+            for (int i = 1; i < register.length; i++) {
+                if (tapPositions[i] == 1) {
+                    register[i] = register[i] ^ 1;
+                }
+            }
+        }
+
+        
+        return outputBit;
     }
+}
+
+
+
+// public class LFSR {
+    
+//     private int tapPositions; // Позиции отводов (задаются полиномом)
+
+//     private int register; // Значения регистра
+    
+
+//     public LFSR(int tapPositions, int register) {
+//         this.tapPositions = tapPositions;
+//         this.register = register;
+        
+//     }
+
+//     public int getNextBit() {
+//         int digits = (int) Math.log10(register) + 1; //определяем количество цифр в регистре
+
+//         int output = register & (1 << (digits - 1)); //получаем число, у которогопоследний бит будет равен последнему нашего регистра
+//         output = output >> (digits - 1); //сдвигаем полученный бит в начало, чтобы получить фактический последний бит регистра
+
+//         // Если последний бит равен 1, то сдвигаем биты регистра вправо, вставляя в старший бит 1 и выполняем XOR
+//         if (output == 1){
+//             register >>>= 1 | 0x1;
+//             register = register ^ tapPositions;
+//         }
+//         // если 0, то просто сдвигаем биты, а новый старший бит заполнится нулем
+//         else if(output == 0) register >>>= 1;
+        
+//         // возвращаем последний бит
+//         return output;
+//     }
 
     
-}
+// }
--- 5/Lab5.java
+++ 5/Lab5.java
@@ -1,31 +1,130 @@
-import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Lab5 {
-    public static void main(String[] args) {
-        String originalImagePath = new File("tux.bmp").getPath();
-        String modifiedImagePath = new File("tuxEmbed.bmp").getPath();
+public class Lab5 extends JFrame{
 
-       
+    private static final int WINDOW_WIDTH = 800; // Ширина окна
+    private static final int WINDOW_HEIGHT = 400; // Высота окна
+    private static final int CIRCLE_RADIUS = 4; // Радиус точки значения регистра
+
+    private static int numBits; // Максимальная длина (период) последовательности
+    private static int[] sequence; // Последовательность максимальной длины
+
+    private static LFSR lfsr;
+    private static int [] polynomial = {1,1,0,0,1}; //(для x^4 + x + 1)
+    private static int [] initialValue = {1,0,0,0}; 
+    // private static int polynomial = 0x11001; //(для x^4 + x + 1)
+    // private static int initialValue = 0x1000; 
+
+
+    private static String originalImagePath = new File("tux.bmp").getPath();
+    private static String encryptedImagePath = new File("tuxEmbed.bmp").getPath();
+    private static String decryptedImagePath = new File("tuxDecr.bmp").getPath();
     
-//------------------------------
-//  Итог: теперь полученный результат стал больше похож на правду (можно увидеть в текущей версии коммита в файле "tuxEmbed"),
-//  но возможно при работе с интовыми значениями что-то пошло не так
-//  Вывод: попробовать использовать значения полинома и регистра, чтобы было проще выделять последний бит регистра и проводить XOR
-//------------------------------
-        try {
-            // Чтение исходного файла изображения
-            FileInputStream inputStream = new FileInputStream(originalImagePath);
-            byte[] header = new byte[120];
-            inputStream.read(header);
-            byte[] imageBytes = new byte[inputStream.available()];
-            inputStream.read(imageBytes);
-            inputStream.close();
-            
+    public static void main(String[] args) {
+        Lab5 main = new Lab5();
+        main.setVisible(true);
+    }
 
-            int polynomial = 0x11001; //(для x^4 + x + 1)
-            int initialValue = 0x1000; 
-            LFSR lfsr = new LFSR(initialValue, polynomial);
-            
+    public Lab5() {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите коэффициенты полинома (разделенные пробелами)");
+        System.out.println("Например: 1 1 0 0 1 (для x^4 + x + 1)");
+        polynomial = toIntArray(in.nextLine());
+
+        System.out.println("Введите начальное значение регистра сдвига (в двоичном формате)");
+        System.out.println("Например: 1000");
+        initialValue = toIntArray(in.nextLine());
+
+        System.out.println("Введите количество периодов последовательности для отображения на диаграмме:");
+        int periods = Integer.valueOf(in.nextLine());
+
+        in.close();
+        
+        // Получаем максимальную длину периода
+        numBits = (int) Math.pow(2, Integer.SIZE - Integer.numberOfLeadingZeros(
+                Integer.parseInt(
+                        Arrays.toString(initialValue).replaceAll("\\[|\\]|,|\\s", ""),
+                        2)))
+                - 1;
+                
+        numBits *= periods;
+
+        lfsr = new LFSR(polynomial, initialValue);
+        sequence = new int[numBits]; // Последовательность бит регистра
+        String seqStr = "";
+        int[] bitFrequency = new int[2]; // Частота 0 и 1 для оценки критерием χ^2
+        for (int i = 0; i < numBits; i++) {
+            int bit = lfsr.getNextBit();
+            sequence[i] = bit;
+            bitFrequency[bit]++;
+            seqStr += bit;
+        }
+
+        // Вычисляем значение критерия χ^2
+        double expectedFrequency = (double) numBits / 2;
+        double chiSquare = 0;
+        for (int i = 0; i < 2; i++) {
+            chiSquare += Math.pow(bitFrequency[i] - expectedFrequency, 2) / expectedFrequency;
+        }
+
+        // Число степеней свободы = кол-во групп - 1 = 2 - 1
+        // Примем уровень значимости = 0.05
+        double criticalValue = 3.8; // Табличное критическое значение χ^2
+
+        // Сравниваем наблюдаемое значение χ^2 с критическим
+        String quality = "";
+        if (chiSquare < criticalValue) {
+            // Последовательность является более качественной
+            quality = "Последовательность соответствует нормальному распределению на уровне значимости 0.05.";
+        } else {
+            // Последовательность имеет более низкое качество
+            quality = "Последовательность значительно отклоняется от нормального распределения на уровне значимости 0.05.";
+        }
+        System.out.println("Последовательность: " + seqStr +
+                "\nX^2 (набл.): " + chiSquare + " | X^2 (теор.): " + criticalValue + "\n" + quality);
+
+        encryptImage(originalImagePath, encryptedImagePath); //применяем XOR
+        encryptImage(encryptedImagePath, decryptedImagePath); //снимаем XOR
+
+
+        // Параметры диаграммы
+        setTitle("Точечная диаграмма РСЛОС");
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        JLabel label1 = new JLabel("Последовательность: " + seqStr);
+        JLabel label2 = new JLabel("X^2 (теор.): " + criticalValue);
+        JLabel label3 = new JLabel("X^2 (набл.): " + Math.round(chiSquare * 1000.0) / 1000.0);
+        JLabel label4 = new JLabel(quality);
+        label1.setBounds(50, 25, 750, 30);
+        label2.setBounds(50, 45, 300, 30);
+        label3.setBounds(50, 65, 300, 30);
+        label4.setBounds(50, 85, 750, 30);
+        getContentPane().add(label1);
+        getContentPane().add(label2);
+        getContentPane().add(label3);
+        getContentPane().add(label4);
+        
+        
+    }
+
+    public static void encryptImage(String inputFilePath, String outputFilePath) {
+        try (FileInputStream fis = new FileInputStream(inputFilePath);
+                FileOutputStream fos = new FileOutputStream(outputFilePath);) {
+            byte[] header = new byte[122];
+            fis.read(header);
+            byte[] imageBytes = new byte[fis.available()];
+            fis.read(imageBytes);
+            fis.close();
 
             byte[] modifiedBytes = new byte[imageBytes.length];
             lfsr = new LFSR(polynomial, initialValue);
@@ -41,19 +140,45 @@ public class Lab5 {
                 modifiedBytes[i] = resultByte;
             }
 
-
-            
-            
-            FileOutputStream outputStream1 = new FileOutputStream(modifiedImagePath);
-            outputStream1.write(header);
-            outputStream1.write(modifiedBytes);
-            outputStream1.close();
-
+            fos.write(header);
+            fos.write(modifiedBytes);
+            fos.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        int shiftPx = 25;
+        g2d.drawLine(0, WINDOW_HEIGHT - shiftPx, WINDOW_WIDTH - shiftPx, WINDOW_HEIGHT - shiftPx); // X
+        g2d.drawLine(CIRCLE_RADIUS + shiftPx, 2 * shiftPx, CIRCLE_RADIUS + shiftPx, WINDOW_HEIGHT); // Y
+
+        for (int i = 0, x = 2 * shiftPx; i < numBits; i++) {
+            int bit = sequence[i];
+            if (bit == 0) {
+                g2d.setColor(Color.BLUE);
+            } else {
+                g2d.setColor(Color.RED);
+            }
+            int y = WINDOW_HEIGHT - CIRCLE_RADIUS - (bit * shiftPx) - shiftPx;
+            Shape circle = new Ellipse2D.Double(x, y, CIRCLE_RADIUS, CIRCLE_RADIUS);
+            g2d.fill(circle);
+            x += ((WINDOW_WIDTH - 2 * shiftPx) / numBits);
+        }
+    }
+
+    public static int[] toIntArray(String string) {
+        char[] charArray = string.replaceAll(" ", "").toCharArray();
+        int[] intArray = new int[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            intArray[i] = Integer.parseInt(charArray[i] + "");
+        }
+
+        return intArray;
+    }
     
 
     
